9fa2eda1c5c77fc5ac5375d2372f2cfe3de0403f
Randyglobal <110671224+Randyglobal@users.noreply.github.com>
2022-12-06 12:20:09 -0800

    Test one in index.html

---
+++ Question 1/index.js
@@ -0,0 +1,24 @@
+
+
+function upperCase(){
+    var name = "snake_case";
+    var secondName = "snake_case"
+ for(var i = 1; i< name.length; i++){
+    var firstCut1 = name.slice(6);
+    var firstCut2 = secondName.slice(0,5);
+    // total = firstCut1 + firstCut2;
+    // return total.join('');
+ }
+ console.log(firstCut1)
+}
+function upperCase(){
+    var name = "snake_case";
+    var secondName = "snake_case"
+ for(var i = 1; i< name.length; i++){
+    var firstCut1 = name.slice(6);
+    var firstCut2 = secondName.slice(0,5);
+    // total = firstCut1 + firstCut2;
+    // return total.join('');
+ }
+ console.log(firstCut1)
+}
---
+++ Question3/index.js
@@ -0,0 +1,8 @@
+var array = [2,7,3,6,4]
+    sum = 0;
+    for(var i = 0; i <array.length; i++){
+        sum += array[i];
+        
+    }
+    
+console.log("This is the sum", sum)
