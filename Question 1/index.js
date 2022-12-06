3b3de67b6649e78d5d32f09e046b72a38c8a7e32
Randyglobal <110671224+Randyglobal@users.noreply.github.com>
2022-12-06 12:31:11 -0800

    test 1 in index.html

--- Question 1/index.js
+++ Question 1/index.js
@@ -1,24 +1,34 @@
 
 
-function upperCase(){
-    var name = "snake_case";
-    var secondName = "snake_case"
- for(var i = 1; i< name.length; i++){
-    var firstCut1 = name.slice(6);
-    var firstCut2 = secondName.slice(0,5);
-    // total = firstCut1 + firstCut2;
-    // return total.join('');
- }
- console.log(firstCut1)
-}
-function upperCase(){
-    var name = "snake_case";
-    var secondName = "snake_case"
- for(var i = 1; i< name.length; i++){
-    var firstCut1 = name.slice(6);
-    var firstCut2 = secondName.slice(0,5);
-    // total = firstCut1 + firstCut2;
-    // return total.join('');
- }
- console.log(firstCut1)
-}
+// function upperCase(){
+//     var name = "snake_case";
+//     var secondName = "snake_case"
+//  for(var i = 1; i< name.length; i++){
+//     var firstCut1 = name.slice(6);
+//     var firstCut2 = secondName.slice(0,5);
+//     // total = firstCut1 + firstCut2;
+//     // return total.join('');
+//  }
+//  console.log(firstCut1)
+// }
+// function upperCase(){
+//     var name = "snake_case";
+//     var secondName = "snake_case"
+//  for(var i = 1; i< name.length; i++){
+//     var firstCut1 = name.slice(6);
+//     var firstCut2 = secondName.slice(0,5);
+//     // total = firstCut1 + firstCut2;
+//     // return total.join('');
+//  }
+//  console.log(firstCut1);
+//}
+
+var nameA= "snake_case";
+var secondName = "snake_case";
+var firstCut1 = nameA.slice(6);
+var firstCut2 = secondName.slice(0,5);
+
+console.log("The result is", firstCut2 + firstCut1);
+
+
+
